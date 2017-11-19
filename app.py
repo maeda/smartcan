7576fe77d9d411710162eb942ca5c597aec99862
Andherson Maeda <maeda.br@gmail.com>
2017-11-19 14:08:50 -0200

    Endpoint to test the model

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import request
+from image_model import classifier
+app = Flask(__name__)
+
+
+@app.route('/image', methods=['POST'])
+def index():
+    if request.method == 'POST':
+        f = request.files['photo']
+        f.save('./photo.jpg')
+    return classifier('./photo.jpg')
---
+++ image_model.py
@@ -0,0 +1,49 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+import numpy as np
+
+K.set_image_dim_ordering('th')
+
+classes_types = {0: 'nonrecyclable', 1: 'reciclable'}
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(3, 150, 150)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='rmsprop',
+    metrics=['accuracy'])
+
+model.load_weights('first_try.h5')
+
+
+def classifier(filename):
+
+    img = image.load_img(filename, target_size=(150, 150))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    images = np.vstack([x])
+    classes = model.predict_classes(images, batch_size=10)
+
+    return classes_types[np.asscalar(classes[0][0])]
