fbbe8ec3d4fdf556ce6bfced746c98f4bfb32a4f
fabiogm <fmagalhaes@gmail.com>
2018-07-05 12:52:31 -0300

    Add feedback to user and refactor.

--- data-collection/reader_picam.py
+++ data-collection/reader_picam.py
@@ -1,31 +1,51 @@
 #!/usr/bin/env python
 
-import cv2
-from picamera.array import PiRGBArray
+import atexit
 from picamera import PiCamera
-from time import sleep
 import RPi.GPIO as GPIO 
+from time import sleep
 
 INPUT_PIN = 18
-counter = 0
+OUTPUT_PIN = 25
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
- 
-camera = PiCamera()
 
-# Allow camera to warm up.
-sleep(2)
+def cleanup_gpio():
+    print('Cleaning up GPIO and exiting...')
+    GPIO.cleanup()
 
-print('Camera ready!')
 
-while True:
-    if GPIO.input(INPUT_PIN) == False:
-        filename = 'photo_' + str(counter) + '.jpg'
-        print(filename)
-        camera.capture(filename)
-        counter += 1
-        sleep(.3)
+def init_pins():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(OUTPUT_PIN, GPIO.OUT)
 
-    sleep(.01)
 
+def main():
+    atexit.register(cleanup_gpio)
+    init_pins()
+
+    # Give feedback to user of camera initialization and warm up. 
+    GPIO.output(OUTPUT_PIN, GPIO.HIGH)
+    
+    camera = PiCamera()
+    sleep(2)
+    
+    print('Camera ready!')
+    GPIO.output(OUTPUT_PIN, GPIO.LOW)
+
+    counter = 0
+    while True:
+        if GPIO.input(INPUT_PIN) == False:
+            GPIO.output(OUTPUT_PIN, GPIO.HIGH)
+            filename = 'photo_' + str(counter) + '.jpg'
+            print(filename)
+            camera.capture(filename)
+            GPIO.output(OUTPUT_PIN, GPIO.LOW)
+            counter += 1
+            sleep(.3)
+    
+        sleep(.01)
+
+
+if __name__ == '__main__':
+    main()
